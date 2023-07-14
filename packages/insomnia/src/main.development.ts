8579b4fd974043dd2ae753a7feab52a810d5503f
James Gatz <jamesgatzos@gmail.com>
2023-07-14 09:01:46 +0200

    Project Presence (#6079)
    * fix ts log issue
    
    * fix ts update
    
    * update fetching of teams
    
    * create and delete remote projects
    
    * real-time
    
    * update api changes
    
    * check for successful response
    
    * get user profile
    
    * cleanup
    
    * update fetch
    
    * update network status
    
    * fix for insomniaFetch new api
    
    * insomnia fetch from window
    
    * fix missing stat
    
    * fix project dropdown
    
    * update avatar
    
    * update presence when closing the app
    
    * update the presence every minute
    
    * cleanup presence
    
    * use eventsource and improve debugging

--- packages/insomnia/src/main.development.ts
+++ packages/insomnia/src/main.development.ts
@@ -8,6 +8,7 @@ import appConfig from '../config/config.json';
 import { changelogUrl, getAppVersion, isDevelopment, isMac } from './common/constants';
 import { database } from './common/database';
 import log, { initializeLogging } from './common/log';
+import { registerInsomniaStreamProtocol } from './main/api.protocol';
 import { registerElectronHandlers } from './main/ipc/electron';
 import { registergRPCHandlers } from './main/ipc/grpc';
 import { registerMainHandlers } from './main/ipc/main';
@@ -22,6 +23,7 @@ import type { ToastNotification } from './ui/components/toast';
 
 initializeSentry();
 
+registerInsomniaStreamProtocol();
 // Handle potential auto-update
 if (checkIfRestartNeeded()) {
   process.exit(0);
